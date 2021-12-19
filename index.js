205599cf756e9bc4457b04f41daa380309f0a534
rt2015 <rtopuz@yahoo.com>
2021-12-19 16:11:13 +0300

    Update index.js

--- index.js
+++ index.js
@@ -4,8 +4,22 @@ const os = require('os');
 var hostname = os.hostname() ; 
 
 app.get('/', function (req, res) {
-    res.send('{ "response": "Hello From rtopuz38----hostname:" + os.hostname() + " request.host.name :" + req.headers.host }');
-    console.log("hostname" ,hostname);
+    
+    
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.write("------" HELLO WORLD RTOPUZ ---------\n");
+
+headers = JSON.stringify(req.headers)
+
+  for (var k in env) {
+    res.write(k + ": " + env[k] + "\n");
+  }
+  res.write(headers );
+    res.write("------hostname :" + hostname );
+  res.end();
+
+   console.log("hostname" ,hostname);
     
 });
 
