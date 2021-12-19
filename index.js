b5e20f487e0d30d4b6ba140d3a0d1eac5b5e74c7
rt2015 <rtopuz@yahoo.com>
2021-12-19 15:46:27 +0300

    Update index.js

--- index.js
+++ index.js
@@ -16,4 +16,4 @@ app.get('/ready', function (req, res) {
     res.send('{ "response": " Great!, It works!" }');
 });
 var server = app.listen(process.env.PORT || 3000);
-module.exports = app,server;
+module.exports = server;
