28d05e46298961f7fafb2624a70062f2df8a821d
rt2015 <rtopuz@yahoo.com>
2021-12-19 15:20:01 +0300

    Update index.test.js

--- src/index.test.js
+++ src/index.test.js
@@ -1,7 +1,12 @@
 var request = require('supertest');
-var app = require('../index.js');
+var app,server = require('../index.js');
 describe('GET /will', function() {
     it('respond with hello world', function(done) {
         request(app).get('/will').expect('{ "response": "Hello World" }', done);
     });
 }); 
+afterAll(done => {
+  // Closing the DB connection allows Jest to exit successfully.
+  server.close()
+  done()
+})
