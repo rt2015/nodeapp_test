8324a5dd2b8ab4389f58c454725b30b238a1d1a0
rt2015 <rtopuz@yahoo.com>
2021-12-19 15:04:36 +0300

    Update index.test.js

--- src/index.test.js
+++ src/index.test.js
@@ -4,4 +4,4 @@ describe('GET /will', function() {
     it('respond with hello world', function(done) {
         request(app).get('/will').expect('{ "response": "Hello World" }', done);
     });
-});
+}); 
