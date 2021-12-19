15bafbc7bb68d86f4d506a7d1b61669906029f09
rt2015 <rtopuz@yahoo.com>
2021-12-19 15:34:44 +0300

    Update index.test.js

--- src/index.test.js
+++ src/index.test.js
@@ -1,5 +1,5 @@
 var request = require('supertest');
-var app,server = require('../index.js');
+var {app,server} = require('../index.js');
 describe('GET /will', function() {
     it('respond with hello world', function(done) {
         request(app).get('/will').expect('{ "response": "Hello World" }', done);
